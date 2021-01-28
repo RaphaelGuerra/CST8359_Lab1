2919bd8b806db0cb86a28d2c06034da4ad39deef
Raphael Guerra <guer0161@algonquinlive.com>
2021-01-28 17:25:20 -0500

    lab1_done

--- Program.cs
+++ Program.cs
@@ -12,55 +12,40 @@ namespace Lab1
         public static IList<string> words = new List<string>();
         static void Main(string[] args)
         {
-            bool temp = true;
+            bool loop = true;
             do
             {
                 Console.WriteLine("Hello World!!! My First C# App");
                 Console.WriteLine("Options\n----------");
-                Console.WriteLine("1 - Import Words From File\n2 - Bubble Sort words\n3 - LINQ/Lambda sort words\n4 - Count the Distinct Words\n5 - Take the first 10 words\n6 - Get the number of words that start with 'j' and display the count\n7 - Get and display of words that end with 'd' and display the count\n8 - Get and display of words that are greater than 4 characters long, and display the count\n9 - Get and display of words that are less than 3 characters long and start with the letter 'a', and display the count\nx – Exit\n");
+                Console.WriteLine("1 - Import Words From File" +
+                    "\n2 - Bubble Sort words" +
+                    "\n3 - LINQ/Lambda sort words" +
+                    "\n4 - Count the Distinct Words" +
+                    "\n5 - Take the first 10 words" +
+                    "\n6 - Get the number of words that start with 'j' and display the count" +
+                    "\n7 - Get and display of words that end with 'd' and display the count" +
+                    "\n8 - Get and display of words that are greater than 4 characters long, and display the count" +
+                    "\n9 - Get and display of words that are less than 3 characters long and start with the letter 'a', and display the count" +
+                    "\nx – Exit\n");
                 Console.WriteLine("Make a selection: ");
-                string select = Console.ReadLine();
+                string userIn = Console.ReadLine();
                 Console.Clear();
-                switch (select)
+                switch (userIn)
                 {
-                    case "1":
-                        GetFile(words);
-                        Console.WriteLine("Reading Words complete");
-                        Console.WriteLine("Number of words found: " + words.Count() + "\n");
-                        break;
-                    case "2":
-                        BubbleSort(words);
-                        break;
-                    case "3":
-                        LinqSort();
-                        break;
-                    case "4":
-                        CountDistinctWords();
-                        break;
-                    case "5":
-                        TakeFirst10Words();
-                        break;
-                    case "6":
-                        GetJWords();
-                        break;
-                    case "7":
-                        GetDisplayDWords();
-                        break;
-                    case "8":
-                        GetDisplayMoreThan4CharWords();
-                        break;
-                    case "9":
-                        GetDisplayLessThan3CharWords();
-                        break;
-                    case "x":
-                        temp = false;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input");
-                        break;
+                    case "1": GetFile(words); break;
+                    case "2": BubbleSort(words); break;
+                    case "3": LinqSort(); break;
+                    case "4": CountDistinctWords(); break;
+                    case "5": TakeFirst10Words(); break;
+                    case "6": GetJWords(); break;
+                    case "7": GetDisplayDWords(); break;
+                    case "8": GetDisplayMoreThan4CharWords(); break;
+                    case "9": GetDisplayLessThan3CharWords(); break;
+                    case "x": loop = false; break;
+                    default: Console.WriteLine("Invalid input"); break;
                 }
                 Console.WriteLine();
-            } while (temp);
+            } while (loop);
         }
         public static void GetFile(IList<string> words)
         {
@@ -82,11 +67,13 @@ namespace Lab1
                 Console.WriteLine("Invalid file");
                 Console.WriteLine(e.Message);
             }
+            Console.WriteLine("Reading Words complete");
+            Console.WriteLine("Number of words found: " + words.Count() + "\n");
         }
         public static IList<string> BubbleSort(IList<string> words)
         {
-            var tempList = words.ToList();
-            int n = tempList.Count();
+            var tempListA = words.ToList();
+            int x = tempListA.Count();
             Stopwatch timer = new Stopwatch();
             timer.Start();
             var swapped = true;
@@ -95,15 +82,15 @@ namespace Lab1
                 while (swapped)
                 {
                     swapped = false;
-                    for (var i = 0; i < n - 1; i++)
+                    for (var i = 0; i < x - 1; i++)
                     {
-                        var left = tempList[i];
-                        var right = tempList[i + 1];
-                        if (left.CompareTo(right) > 0)
+                        var l = tempListA[i];
+                        var r = tempListA[i + 1];
+                        if (l.CompareTo(r) > 0)
                         {
-                            var temp = tempList[i];
-                            tempList[i] = tempList[i + 1];
-                            tempList[i + 1] = temp;
+                            var ListB = tempListA[i];
+                            tempListA[i] = tempListA[i + 1];
+                            tempListA[i + 1] = ListB;
                             swapped = true;
                         }
                     }
@@ -117,14 +104,14 @@ namespace Lab1
             timer.Stop();
             TimeSpan sec = timer.Elapsed;
             Console.Write("Time elasped: " + sec.Milliseconds + " ms" + "\n\n");
-            return tempList;
+            return tempListA;
         }
         public static void LinqSort()
         {
             Stopwatch sec = new Stopwatch();
             sec.Start();
-            var total = from words in words orderby words.ToString() select words;
-            foreach (var i in total) { }
+            var s = from element in words orderby element.ToString() select element;
+            foreach (var i in s)
             sec.Stop();
             TimeSpan time = sec.Elapsed;
             Console.Write("Time elapsed: " + time.Milliseconds + " ms" + "\n\n");
@@ -132,70 +119,70 @@ namespace Lab1
         }
         public static void CountDistinctWords()
         {
-            var num = words.Count();
-            num = 0;
-            var dn = words.Distinct();
-            foreach (var i in dn)
+            var t = words.Count();
+            t = 0;
+            var s = words.Distinct();
+            foreach (var i in s)
             {
-                num++;
+                t++;
             }
-            Console.Write("The number of distinct words is: " + num + "\n\n");
+            Console.Write("The number of distinct words is: " + t + "\n\n");
         }
         public static void TakeFirst10Words()
         {
-            var ten = words.Take(10);
-            foreach (var i in ten)
+            var s = words.Take(10);
+            foreach (var i in s)
             {
                 Console.WriteLine(i);
             }
         }
         public static void GetJWords()
         {
-            var num = words.Count();
-            num = 0;
-            var jstart = from words in words where words.StartsWith("j") select words;
-            foreach (var i in jstart)
+            var t = words.Count();
+            t = 0;
+            var s = from words in words where words.StartsWith("j") select words;
+            foreach (var i in s)
             {
-                num++;
+                t++;
                 Console.WriteLine(i);
             }
-            Console.Write("Number of words that start with 'j': " + num + "\n\n");
+            Console.Write("Number of words that start with 'j': " + t + "\n\n");
         }
         public static void GetDisplayDWords()
         {
-            var num = words.Count();
-            num = 0;
-            var Dend = from words in words where words.EndsWith("d") select words;
-            foreach (var i in Dend)
+            var t = words.Count();
+            t = 0;
+            var s = from words in words where words.EndsWith("d") select words;
+            foreach (var i in s)
             {
-                num++;
+                t++;
                 Console.WriteLine(i);
             }
-            Console.Write("Number of words that end with 'd': " + num + "\n\n");
+            Console.Write("Number of words that end with 'd': " + t + "\n\n");
         }
         public static void GetDisplayMoreThan4CharWords()
         {
-            var num = words.Count();
-            num = 0;
-            var four = from words in words where words.Length > 4 select words;
-            foreach (var i in four)
+            var t = words.Count();
+            t = 0;
+            var s = from words in words where words.Length > 4 select words;
+            foreach (var i in s)
             {
-                num++;
+                t++;
                 Console.WriteLine(i);
             }
-            Console.Write("Number of words longer than 4 characters:" + num + "\n\n");
+            Console.Write("Number of words longer than 4 characters:" + t + "\n\n");
         }
         public static void GetDisplayLessThan3CharWords()
         {
-            var num = words.Count();
-            num = 0;
-            var three = from words in words where words.StartsWith("a") && words.Length < 3 select words;
-            foreach (var i in three)
+            var t = words.Count();
+            t = 0;
+            var s = from words in words where words.StartsWith("a") && words.Length < 3 select words;
+            foreach (var i in s)
             {
-                num++;
+                t++;
                 Console.WriteLine(i);
             }
-            Console.Write("Number of words longer less than 3 characters and start with 'a': " + num + "\n\n");
+            Console.Write("Number of words longer less than 3 characters and start with 'a': " + t + "\n\n");
         }
     }
 }
